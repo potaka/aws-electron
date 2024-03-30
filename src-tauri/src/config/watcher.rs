55246808220cffffc122e37f59bd57a9c4f72221
Philip Dowie <philip@jnawk.nz>
2024-03-31 00:58:45 +1300

    added config file watcher

---
+++ src-tauri/src/config/watcher.rs
@@ -0,0 +1,55 @@
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt, StreamExt,
+};
+use notify::{event::{AccessKind, AccessMode}, Config, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::PathBuf;
+use tauri::{AppHandle, Manager};
+
+fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let watcher = RecommendedWatcher::new(
+        move |res| {
+            futures::executor::block_on(async {
+                tx.send(res).await.unwrap();
+            })
+        },
+        Config::default(),
+    )?;
+
+    Ok((watcher, rx))
+}
+
+pub async fn async_watch(app_handle: AppHandle) -> notify::Result<()> {
+    let (mut watcher, mut rx) = async_watcher()?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(super::get_aws_config_dir().as_ref(), RecursiveMode::NonRecursive)?;
+
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(event) => {
+                match event.kind {
+                    EventKind::Access(AccessKind::Close(AccessMode::Write)) => {
+                        let changed_file: &PathBuf = &event.paths[0];
+                        if changed_file.file_name().unwrap() == "config" {
+                            app_handle.trigger_global(
+                                "file-change",
+                                Some(String::from(changed_file.file_name().unwrap().to_str().unwrap())),
+                            )
+                        }
+
+                    },
+                    _ => {}
+                }
+            },
+            _ => {},
+        }
+    }
+    Ok(())
+}
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,10 +4,34 @@
 mod commands;
 mod config;
 mod menu;
+use tauri::{
+    AppHandle,
+    Manager,
+    Window,
+    async_runtime,
+};
+async fn send_aws_config(
+    app: AppHandle,
+) {
+    let launcher_window: Window = app.get_window("launcher").unwrap();
+    let config = commands::load_config().await;
+    _ = launcher_window.emit("new-config", config);
 }
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.app_handle();
+
+            // register a listener for changes to the config file
+            app.listen_global("file-change", move |_| {
+                async_runtime::spawn(send_aws_config(app_handle.clone()));
+            });
+
+            // watch for changes to the config file
+            async_runtime::spawn(config::watcher::async_watch(app.app_handle()));
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             commands::open_mfa_cache,
             commands::load_config,
