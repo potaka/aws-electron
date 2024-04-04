2d64508f0a3b59f84a0192d7e33554294f84d392
Philip Dowie <philip@jnawk.nz>
2024-04-05 01:39:04 +1300

    ignore unused vars starting with _

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,4 +5,19 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "@electron-toolkit/eslint-config-ts/recommended",
     "@electron-toolkit/eslint-config-prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 }
